9579a1a15de99e7b7ad856e581caf41b305862a5
Badikara! <45030782+bagasadkr02@users.noreply.github.com>
2023-02-13 14:24:08 +0700

    Update index.php

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -1,22 +1,23 @@
-<?php 
+<?php
 if (!empty($_SESSION)) {
 	if ($_SESSION['level'] !== 'admin') {
 		echo "<script>history.back();</script>";
 	}
-}else{
-	echo "<script>window.location.href = '".BASEURL."/user/login';</script>";
+} else {
+	echo "<script>window.location.href = '" . BASEURL . "/user/login';</script>";
 }
 
 
 
 
- ?>
+?>
 <style>
-	body{
+	body {
 		background-image: url('<?= BASEURL; ?>/img/hero-bg.jpg');
 	}
 </style>
- 
+
 <div class="container d-flex" align="center" style="justify-content: center; margin-top: 40vh;">
 	<h1>Selamat Datang Admin</h1>
+	<h1>aku ganteng</h1>
 </div>
