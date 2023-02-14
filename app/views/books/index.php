f4481d90cf2c33263556209f7c2b0a3d058b0c3a
Kreutxer <danihorison65@gmail.com>
2023-02-14 23:45:18 +0700

    Update index.php

--- app/views/books/index.php
+++ app/views/books/index.php
@@ -1,121 +1,59 @@
 <div class="container">
 
-	<div class="card d-flex flex-row flex-wrap justify-content-center" style="margin-bottom: 100px;">
-		<?php $id_bf = 0; ?>
-		<?php foreach ($data['buku'] as $b): ?>
-
-			<!-- <div class="mt-2 mb-2 shadow  bg-body-tertiary rounded"
-													style="width: 15vw; margin-left: 1vw; margin-right: 1vw;">
-			<ul class="list-group" style="margin: 5px;">
-				<img src="<?= BASEURL; ?>/img/<?= $b['cover']; ?>" alt="">
-				<p class="list-group"><b>Judul : </b>
-					<?= $b['judul']; ?>
-				</p>
-				<p class="list-group"><b>Harga : </b>Rp.
-					<?= $b['harga']; ?>
-				</p>
-				<p class="list-group" style=""><b> Deskripsi : </b>
-					<?= substr($b['deskripsi'], 0, 8); ?> ...
-				</p>
-				<br>
-			</ul>
-			<p class="list-group small mb-1" align="left">
-				<a href="<?= BASEURL; ?>/books/author/<?= $b['id_user']; ?>" style="text-decoration: none;">
-					<?= $b['nama']; ?>
-				</a>
-			</p>
-			<div align="right" class="mb-1">
-				<button type="button" class="btn btn-outline-danger" data-bs-toggle="modal"
-					data-bs-target="#modal-detail" id="detail" style="width: 2.5vw; height: 3.5vh; background-image: url('<?= BASEURL ?>/img/fav.png');
-					background-size: 1.5vw; background-repeat: no-repeat; background-position: center;
-					<?php if (!isset($_SESSION['login'])) {
-						echo 'display: none;';
-					} ?>
-					<?php for ($i = 0; $i < count($data['favorit']); $i++) {
-						if ($data['favorit'][$i]['id_buku'] === $b['id_buku']) {
-							$id_bf = $data['favorit'][$i]['id_buku'];
-							echo 'display: none;';
-						}
-					} ?>" data-idbuku="<?= $b['id_buku']; ?>" data-iduser="<?= $b['id_user']; ?>" data-judul="<?= $b['judul']; ?>"
-					data-harga="<?= $b['harga']; ?>" data-no="<?= $b['no_telp']; ?>"
-					data-deskripsi="<?= $b['deskripsi']; ?>" data-cover="<?= $b['cover']; ?>"
-					data-nama="<?= $b['nama']; ?>" data-bukuFav="<?= $id_bf; ?>">
-				</button>
-				<?php for ($i = 0; $i < count($data['favorit']); $i++) {
-					if ($data['favorit'][$i]['id_buku'] === $b['id_buku']) {
-						echo '*favoritmu';
-					}
-				} ?>
-			</div>
-			<button type="button" class="btn btn-primary mb-1" data-bs-toggle="modal" data-bs-target="#modal-detail"
-				id="detail" data-idbuku="<?= $b['id_buku']; ?>" data-iduser="<?= $b['id_user']; ?>"
-				data-judul="<?= $b['judul']; ?>" data-harga="<?= $b['harga']; ?>" data-no="<?= $b['no_telp']; ?>"
-				data-deskripsi="<?= $b['deskripsi']; ?>" data-cover="<?= $b['cover']; ?>" data-nama="<?= $b['nama']; ?>"
-				data-bukufav="<?= $id_bf; ?>">
-				Lihat Detail..
-			</button>
-			<button class="btn btn-light" style="background-color: rgb(37,211,102); ">
-				<a href="https://api.whatsapp.com/send?phone=62<?= $b['no_telp']; ?>"
-					style="color: white; text-decoration: none;" target="blank">Chat Ke WA</a>
-							</button>
-						</div> -->
-
-			<div class="height d-flex justify-content-center align-items-center">
-
-				<div class="container mt-5 mb-5">
-					<div class="d-flex justify-content-center row">
-						<div class="col-md-10">
-							<div class="row p-2 bg-white border rounded">
-								<div class="col-md-3 mt-1"><img class="img-fluid img-responsive rounded product-image"
-										src="<?= BASEURL; ?>/img/<?= $b['cover']; ?>"></div>
-								<div class="col-md-6 mt-1">
-									<h5>
-										<?= $b['judul']; ?>
-									</h5>
-									<p class="text-justify para mb-0">
-										<?= $b['deskripsi']; ?><br><br>
-									</p>
-									<a href="<?= BASEURL; ?>/books/author/<?= $b['id_user']; ?>"
-										style="text-decoration: none;">
-										<?= $b['nama']; ?>
-									</a>
+	<?php $id_bf = 0; ?>
+	<?php foreach ($data['buku'] as $b): ?>
+		<div class="height d-flex justify-content-center align-items-center">
+			<div class="container mt-5 mb-5">
+				<div class="d-flex justify-content-center row">
+					<div class="col-md-10">
+						<div class="row p-2 bg-white border rounded">
+							<div class="col-md-3 mt-1"><img class="img-fluid img-responsive rounded product-image"
+									src="<?= BASEURL; ?>/img/<?= $b['cover']; ?>"></div>
+							<div class="col-md-6 mt-1">
+								<h5>
+									<?= $b['judul']; ?>
+								</h5>
+								<p class="text-justify para mb-0">
+									<?= $b['deskripsi']; ?><br><br>
+								</p>
+								<a href="<?= BASEURL; ?>/books/author/<?= $b['id_user']; ?>" style="text-decoration: none;">
+									<?= $b['nama']; ?>
+								</a>
+							</div>
+							<div class="align-items-center align-content-center col-md-3 border-left mt-1">
+								<div class="d-flex flex-row align-items-center">
+									<h4 class="mr-1">Rp.
+										<?= number_format($b['harga'], 2); ?>
+									</h4>
 								</div>
-								<div class="align-items-center align-content-center col-md-3 border-left mt-1">
-									<div class="d-flex flex-row align-items-center">
-										<h4 class="mr-1">Rp.
-											<?= number_format($b['harga'], 2); ?>
-										</h4>
-									</div>
-									<h6 class="text-success">Cash on Delivery</h6>
-									<div class="d-flex flex-column mt-4">
-										<button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal"
-											data-bs-target="#modal-detail" id="detail" data-idbuku="<?= $b['id_buku']; ?>"
-											data-iduser="<?= $b['id_user']; ?>" data-judul="<?= $b['judul']; ?>"
-											data-harga="<?= $b['harga']; ?>" data-no="<?= $b['no_telp']; ?>"
-											data-deskripsi="<?= $b['deskripsi']; ?>" data-cover="<?= $b['cover']; ?>"
-											data-nama="<?= $b['nama']; ?>" data-bukufav="<?= $id_bf; ?>">
-											Lihat Detail
-										</button>
-									</div>
-									<div class="d-flex flex-column mt-1">
-										<button class="btn btn-light btn-sm" style="background-color: rgb(37,211,102); ">
-											<a href="https://api.whatsapp.com/send?phone=62<?= $b['no_telp']; ?>"
-												style="color: white; text-decoration: none;" target="blank">Chat Ke
-												WA</a>
-										</button>
-									</div>
+
+								<h6 class="text-success">Cash on Delivery</h6>
+								<div class="d-flex flex-column mt-4">
+									<button type="button" class="btn btn-primary btn-sm" id="detail" data-idbuku="<?= $b['id_buku']; ?>"
+										data-iduser="<?= $b['id_user']; ?>" data-judul="<?= $b['judul']; ?>"
+										data-harga="<?= $b['harga']; ?>" data-no="<?= $b['no_telp']; ?>"
+										data-deskripsi="<?= $b['deskripsi']; ?>" data-cover="<?= $b['cover']; ?>"
+										data-nama="<?= $b['nama']; ?>" data-bukufav="<?= $id_bf; ?>">
+										Lihat Detail
+									</button>
+								</div>
+								<div class="d-flex flex-column mt-1">
+									<button class="btn btn-light btn-sm" style="background-color: rgb(37,211,102); ">
+										<a href="https://api.whatsapp.com/send?phone=62<?= $b['no_telp']; ?>"
+											style="color: white; text-decoration: none;" target="blank">Chat Ke
+											WA
+										</a>
+									</button>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-
 		</div>
-
 	<?php endforeach; ?>
 </div>
-</div>
+
 
 <!-- Modal Box Detail -->
 <div class="modal modal-xl" tabindex="-1" id="modal-detail">
@@ -126,27 +64,30 @@
 				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 			</div>
 			<div class="modal-body">
-				<img src="" alt="" id="cover"><br><br><br>
+
+				<div class="container ">
+					<img src="<?= BASEURL; ?>/img/<?= $b['cover']; ?>" alt="" class="img-fluid" id="cover">
+				</div>
+
 				<div class="penjual" id="penjual">
 					<a href="" style="text-decoration: none;">
 						<h4></h4>
 					</a>
-				</div><br>
-				<input type="hidden" class="form-control" id="id-buku" name="id-buku">
-				<input type="hidden" class="form-control" id="id-user" name="id-user">
+				</div>
+				<img src="" alt="">
 				<div class="input-group flex-nowrap mt-2 mb-2">
-					<span class="input-group-text" id="addon-wrapping">Judul : </span>
-					<input type="text" class="form-control" id="judul" name="judul" readonly>
+					<input type="text" class="form-control" id="judul" name="judul" placeholder="<?= $b['judul']; ?>" readonly>
 				</div>
 				<div class="input-group flex-nowrap mt-2 mb-2">
-					<span class="input-group-text" id="addon-wrapping">harga : </span>
-					<input type="text" class="form-control" id="harga" name="harga" readonly>
+					<h5>
+						Rp.
+						<?= number_format($b['harga'], 2); ?>
+					</h5>
 				</div>
 				<div>
-					<label for="deskripsi">Deskripsi :</label>
-					<textarea class="form-control" id="deskripsi" name="deskripsi" style="height: 100px"
-						readonly></textarea>
-
+					<p>
+						<?= $b['deskripsi']; ?>
+					</p>
 				</div>
 			</div>
 			<div class="modal-footer">
@@ -252,6 +193,7 @@
 
 
 <script>
+	$(document).ready(function(){
 	$(document).on("click", "#detail", function () {
 		$('.modal-footer #favorite').show();
 		const idB = $(this).data('idbuku');
@@ -270,18 +212,19 @@
 		if (idBF === idB) {
 			$('.modal-footer #favorite').hide();
 		}
-		$('.modal-footer #favorite a').attr('href', '<?= BASEURL; ?>/Books/favorite/'+i                           d B);
+		$('.modal-footer #favorite a').attr('href', '<?= BASEURL; ?>/Books/favorite/'+i                                         d B);
 		$('.modal-body #id-buku').val(idB);
 		$('.modal-body #id-user').val(idU);
 		$('.modal-body #penjual a h4').text(nama);
-		$('.modal-body #penjual a').attr("href", '<?= BASEURL; ?>/Books/author/'+i                           d U);
+		$('.modal-body #penjual a').attr("href", '<?= BASEURL; ?>/Books/author/'+i                                         d U);
 		$('.modal-body #judul').val(judul);
 		$('.modal-body #harga').val('Rp.' + harga);
 		$('.modal-body #judul').val(judul);
 		$('.modal-body #deskripsi').val(deskripsi);
 		$('.modal-body #cover').attr("src", '<?= BASEURL; ?>/img/' + c                       o ver);
 		$('.modal-footer #no a').attr("href", 'https://api.whatsapp.com/send?phone=62' + no);
+		
 	});
-
+});
 
 </script>
